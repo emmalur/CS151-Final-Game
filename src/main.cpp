425d5c3205b4545b75c4d71ef23dbef2ad8a70de
Emma Ures <emmau@hawaii.edu>
2024-12-16 20:19:32 -1000

    setup

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Needs if there is no tests defined
+#define DOCTEST_CONFIG_DISABLE
+//#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+
+#include "nlohmann/json.hpp"
+
+int main()
+{
+    std::cout << "testing" << "\n";
+
+    return 0;
+}
